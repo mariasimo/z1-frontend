e13f377be06047b7cbd1f3f646176a9d61ccf6f7
María Simó <maria.simo@secuoyas.com>
2021-02-20 09:32:32 +0100

    clean cra, add formatter, test endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  useEffect(() => {
+    fetch("https://front-exercise.z1.digital/evaluations", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        someRandomVal: "someRandomVal",
+      }),
+    })
+      .then((result) => result.json())
+      .then((result) => console.log(result));
+  }, []);
+
+  return <div className="App">Hey girl</div>;
+};
 
 export default App;
