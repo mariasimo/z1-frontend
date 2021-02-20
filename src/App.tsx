4e582d071a4dca960070a44eb93065c99eb4f383
María Simó <maria.simo@secuoyas.com>
2021-02-20 10:06:17 +0100

    add logic for opening camera and submit the result

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 
 const App = () => {
-  useEffect(() => {
-    fetch("https://front-exercise.z1.digital/evaluations", {
+  const manageCamera = useCallback(() => {
+    const constrains: {
+      video: boolean | MediaTrackConstraints | undefined;
+    } = {
+      video: true,
+    };
+
+    const successCallback: (
+      localMediaStream: MediaStream | undefined
+    ) => void = function (localMediaStream) {
+      console.log("call sucess", localMediaStream);
+      submitImage(localMediaStream);
+    };
+    const errorCallback: (err: MediaStreamError) => void = function (err) {
+      console.log(err);
+    };
+    navigator.getUserMedia(constrains, successCallback, errorCallback);
+  }, []);
+
+  const submitImage: (img: MediaStream | undefined) => void = (img) => {
+    return fetch("https://front-exercise.z1.digital/evaluations", {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        someRandomVal: "someRandomVal",
-      }),
+      body: JSON.stringify({}),
     })
       .then((result) => result.json())
       .then((result) => console.log(result));
-  }, []);
+  };
+
+  useEffect(() => {
+    manageCamera();
+  }, [manageCamera]);
 
   return <div className="App">Hey girl</div>;
 };
